d2addc33692d8b2f249476209d90442264149ea6
Joe Richey joerichey@google.com <joerichey@google.com>
2019-01-02 16:08:44 -0800

    Add test for NewKey

--- tpm2tools/keys_test.go
+++ tpm2tools/keys_test.go
@@ -3,6 +3,9 @@ package tpm2tools
 import (
 	"testing"
 
+	"github.com/google/go-tpm/tpm2"
+	"github.com/google/go-tpm/tpmutil"
+
 	"github.com/google/go-tpm-tools/simulator"
 )
 
@@ -30,5 +33,22 @@ func TestNameMatchesPublicArea(t *testing.T) {
 	}
 	if !matches {
 		t.Fatal("Returned name and computed name do not match")
+	}
+}
+
+func TestCreateSigningKeysInAllHierarchies(t *testing.T) {
+	simulator, err := simulator.Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	template := AIKTemplateRSA([256]byte{})
+	for _, hierarchy := range []tpmutil.Handle{tpm2.HandleOwner, tpm2.HandleEndorsement, tpm2.HandlePlatform, tpm2.HandleNull} {
+		key, err := NewKey(simulator, hierarchy, template)
+		if err != nil {
+			t.Errorf("Hierarchy %+v: %s", hierarchy, err)
+		} else {
+			key.Close()
+		}
 	}
 }
