ed3ffe8b32856e8596f61409e4d04a082bac38f1
Samuel Davidson <samueldavidson@google.com>
2019-01-28 11:55:56 -0800

    Move binary out of /tpm2tools subfolder.

--- tpm2tools/handles.go
+++ tpm2tools/handles.go
@@ -9,23 +9,6 @@ import (
 	"github.com/samdamana/go-tpm/tpm2"
 )
 
-// FlushActiveHandles calls FlushContext() on all active handles within the
-// TPM at io.ReadWriter rw. Returns nil if successful or TPM has no active
-// handles.
-func FlushActiveHandles(rw io.ReadWriter) error {
-	handles, err := Handles(rw, tpm2.HandleTypeTransient)
-	if err != nil {
-		return err
-	}
-	for _, handle := range handles {
-		err = tpm2.FlushContext(rw, handle)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // Handles returns a slice of tpmutil.Handle objects of all handles within
 // the TPM rw of type handleType.
 func Handles(rw io.ReadWriter, handleType tpm2.HandleType) ([]tpmutil.Handle, error) {
--- tpm2tools/handles_test.go
+++ tpm2tools/handles_test.go
@@ -44,34 +44,6 @@ func loadRandomExternalKey(t *testing.T, simulator *simulator.Simulator) {
 	}
 }
 
-func TestFlushActiveHandles(t *testing.T) {
-	simulator, err := simulator.Get()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer simulator.Close()
-
-	// Loads then flushes 1, 2, ...maxHandles handles.
-	for i := 0; i <= maxHandles; i++ {
-		for j := 0; j < i; j++ {
-			loadRandomExternalKey(t, simulator)
-		}
-		err = FlushActiveHandles(simulator)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	// Ensure there are no active handles after all that.
-	h, err := Handles(simulator, tpm2.HandleTypeTransient)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(h) != 0 {
-		t.Fatal("Bah this should be empty!")
-	}
-}
-
 func TestHandles(t *testing.T) {
 	simulator, err := simulator.Get()
 	if err != nil {
