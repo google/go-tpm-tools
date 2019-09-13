59ffafbda4ba2913340a2e25b068ec5e3196b381
Ben Hillier <bhillier@google.com>
2019-09-13 11:51:52 -0700

    update key conversion tests

--- server/key_conversion_test.go
+++ server/key_conversion_test.go
@@ -2,25 +2,40 @@ package server
 
 import (
 	"crypto/rand"
+	"crypto/rsa"
+	"github.com/google/go-tpm-tools/internal"
 	"github.com/google/go-tpm-tools/tpm2tools"
-	"io"
 	"testing"
 )
 
-func TestCreateEKPublicAreaFromKey(t *testing.T) {
-	area := tpm2tools.DefaultEKTemplateRSA()
-	if _, err := io.ReadFull(rand.Reader, area.RSAParameters.ModulusRaw); err != nil {
-		t.Fatal(err)
-	}
-	key, err := area.Key()
+func TestCreateEKPublicAreaFromKeyGeneratedKey(t *testing.T) {
+	template := tpm2tools.DefaultEKTemplateRSA()
+	key, err := rsa.GenerateKey(rand.Reader, int(template.RSAParameters.KeyBits))
 	if err != nil {
 		t.Fatal(err)
 	}
-	newArea, err := CreateEKPublicAreaFromKey(key)
+	newArea, err := CreateEKPublicAreaFromKey(key.Public())
 	if err != nil {
 		t.Fatalf("failed to create public area from public key: %v", err)
 	}
-	if !newArea.MatchesTemplate(area) {
-		t.Errorf("public areas did not match. got: %+v want: %v", newArea, area)
+	if !newArea.MatchesTemplate(template) {
+		t.Errorf("public areas did not match. got: %+v want: %+v", newArea, template)
 	}
+}
+
+func TestCreateEKPublicAreaFromKeyTPMKey(t *testing.T) {
+	rwc := internal.GetTPM(t)
+	defer tpm2tools.CheckedClose(t, rwc)
+
+	ek, err := tpm2tools.EndorsementKeyRSA(rwc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ek.Close()
+	newArea, err := CreateEKPublicAreaFromKey(ek.PublicKey())
+
+	if matches, err := ek.Name().MatchesPublic(newArea); !matches || err != nil {
+		t.Error("public areas did not match.")
+	}
+
 }
