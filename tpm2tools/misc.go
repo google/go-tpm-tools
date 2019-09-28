90c8c9c74d8061797047ed5fa0680910c92721a0
Steve Rutherford <srutherford@google.com>
2019-09-27 19:39:29 -0700

    updated tests

--- tpm2tools/misc.go
+++ tpm2tools/misc.go
@@ -18,28 +18,39 @@ func GetPCRCount(rw io.ReadWriter) (uint32, error) {
 	if len(props) != 1 {
 		return 0, fmt.Errorf("tpm returned unexpected list of properties: %v", props)
 	}
+
 	return props[0].(tpm2.TaggedProperty).Value, nil
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 // ReadPCRs fetchs the values of the specified PCRs for the specified hash.
 func ReadPCRs(rw io.ReadWriter, pcrs []int, hash tpm2.Algorithm) (*proto.Pcrs, error) {
-	pcrSel := tpm2.PCRSelection{
-		Hash: hash,
-		PCRs: pcrs,
-	}
-
-	pcrMap, err := tpm2.ReadPCRs(rw, pcrSel)
-	if err != nil {
-		return nil, err
-	}
-
 	pl := proto.Pcrs{
 		Hash: proto.HashAlgo(hash),
 		Pcrs: map[uint32][]byte{},
 	}
 
-	for pcr, val := range pcrMap {
-		pl.Pcrs[uint32(pcr)] = val
+	for i := 0; i < len(pcrs); i += 8 {
+		end := min(i+8, len(pcrs))
+		pcrSel := tpm2.PCRSelection{
+			Hash: hash,
+			PCRs: pcrs[i:end],
+		}
+
+		pcrMap, err := tpm2.ReadPCRs(rw, pcrSel)
+		if err != nil {
+			return nil, err
+		}
+
+		for pcr, val := range pcrMap {
+			pl.Pcrs[uint32(pcr)] = val
+		}
 	}
 
 	return &pl, nil
--- tpm2tools/misc_test.go
+++ tpm2tools/misc_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"crypto/sha1"
 	"crypto/sha256"
+	"fmt"
+	"hash"
 	"testing"
 
 	"github.com/google/go-tpm-tools/internal"
@@ -12,37 +14,62 @@ import (
 )
 
 var tests = []struct {
-	inPcrs       []int
 	inAlg        tpm2.Algorithm
 	inExtensions [][]byte
-	out          []byte
 }{
-	{[]int{0}, tpm2.AlgSHA1, nil, bytes.Repeat([]byte{0x00}, sha1.Size)},
-	{[]int{0}, tpm2.AlgSHA1, [][]byte{bytes.Repeat([]byte{0x00}, sha1.Size)}, []byte{184, 13, 229, 209, 56, 117, 133, 65, 197, 240, 82, 101, 173, 20, 74, 185, 250, 134, 209, 219}},
-	{[]int{0}, tpm2.AlgSHA256, nil, bytes.Repeat([]byte{0x00}, sha256.Size)},
-	{[]int{0}, tpm2.AlgSHA256, [][]byte{bytes.Repeat([]byte{0x00}, sha256.Size)}, []byte{245, 165, 253, 66, 209, 106, 32, 48, 39, 152, 239, 110, 211, 9, 151, 155, 67, 0, 61, 35, 32, 217, 240, 232, 234, 152, 49, 169, 39, 89, 251, 75}},
+	{tpm2.AlgSHA1, nil},
+	{tpm2.AlgSHA1, [][]byte{bytes.Repeat([]byte{0x00}, sha1.Size)}},
+	{tpm2.AlgSHA1, [][]byte{bytes.Repeat([]byte{0x01}, sha1.Size)}},
+	{tpm2.AlgSHA1, [][]byte{bytes.Repeat([]byte{0x02}, sha1.Size)}},
+	{tpm2.AlgSHA256, nil},
+	{tpm2.AlgSHA256, [][]byte{bytes.Repeat([]byte{0x00}, sha256.Size)}},
+	{tpm2.AlgSHA256, [][]byte{bytes.Repeat([]byte{0x01}, sha256.Size)}},
+	{tpm2.AlgSHA256, [][]byte{bytes.Repeat([]byte{0x02}, sha256.Size)}},
+}
+
+func pcrExtend(alg tpm2.Algorithm, old, new []byte) ([]byte, error) {
+	var h hash.Hash
+	switch alg {
+	case tpm2.AlgSHA1:
+		h = sha1.New()
+	case tpm2.AlgSHA256:
+		h = sha256.New()
+	default:
+		return nil, fmt.Errorf("not a valid hash type: %v", alg)
+	}
+	h.Write(old)
+	h.Write(new)
+	return h.Sum(nil), nil
 }
 
 func TestReadPCRs(t *testing.T) {
 	rwc := internal.GetTPM(t)
 	defer CheckedClose(t, rwc)
 
+	testPcrs := make(map[tpm2.Algorithm][]byte, 2)
+	testPcrs[tpm2.AlgSHA1] = bytes.Repeat([]byte{0x00}, sha1.Size)
+	testPcrs[tpm2.AlgSHA256] = bytes.Repeat([]byte{0x00}, sha256.Size)
+
 	for _, test := range tests {
 		for _, extension := range test.inExtensions {
 			err := tpm2.PCRExtend(rwc, tpmutil.Handle(0), test.inAlg, extension, "")
-
 			if err != nil {
 				t.Fatalf("failed to extend pcr for test %v", err)
 			}
+
+			testPcrs[test.inAlg], err = pcrExtend(test.inAlg, testPcrs[test.inAlg], extension)
+			if err != nil {
+				t.Fatalf("could not extend pcr: %v", err)
+			}
 		}
 
-		proto, err := ReadPCRs(rwc, test.inPcrs, test.inAlg)
+		proto, err := ReadPCRs(rwc, []int{0}, test.inAlg)
 		if err != nil {
 			t.Fatalf("failed to read pcrs %v", err)
 		}
 
-		if !bytes.Equal(proto.Pcrs[0], test.out) {
-			t.Fatalf("%v not equal to expected %v", proto.Pcrs[0], test.out)
+		if !bytes.Equal(proto.Pcrs[0], testPcrs[test.inAlg]) {
+			t.Fatalf("%v not equal to expected %v", proto.Pcrs[0], testPcrs[test.inAlg])
 		}
 	}
 }
@@ -58,5 +85,4 @@ func TestGetPCRCount(t *testing.T) {
 	if pcrCount != 24 {
 		t.Fatalf("tpm simulator has unexpected number of PCRs: %v instead of 24", pcrCount)
 	}
-
 }
