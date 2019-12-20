69851c7082395ce69e0dcd71ccd0a9f2c9dd8ae5
Joe Richey <joerichey@google.com>
2019-12-20 14:08:35 -0800

    boot-unseal: Keep track of unlocked status
    This avoids trying keys and mounting them if we have already unsealed
    a key. We also print an error message if we failed to unseal any keys.

--- files/boot-unseal.sh
+++ files/boot-unseal.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/ash
 
+unlocked=0
 # Loop through all devices to find the ESP
 for device in $(blkid -o device); do
     part_type=$(blkid -p $device -s PART_ENTRY_TYPE -o value)
-    if [[ "$part_type" != "c12a7328-f81f-11d2-ba4b-00a0c93ec93b" ]]; then
+    if [ "$part_type" != "c12a7328-f81f-11d2-ba4b-00a0c93ec93b" ]; then
         continue
     fi
 
@@ -15,9 +16,19 @@ for device in $(blkid -o device); do
     for f in /mnt/esp/*/disk_unlock_keys/*.sealed; do
         if gotpm unseal --input "$f" --output "/crypto_keyfile.bin" ; then
             echo "gotpm: Unsealed $f"
+            unlocked=1
+            break
         fi
     done
     umount $device
+
+    if [ $unlocked -ne 0 ]; then
+        break
+    fi
 done
+
+if [ $unlocked -eq 0 ]; then
+    echo "gotpm: Unable to unseal TPM disk unlock key"
+fi
 
 # vim: set ft=sh ts=4 sw=4 et:
