3d60f2ea79b028ca3c6ceb568e7867ddab00edd8
Joe Richey <joerichey@google.com>
2019-12-20 16:05:40 -0800

    boot-unseal: Add comments and file check
    We want to avoid dumping errors if there are not keys in the directory

--- files/boot-unseal.sh
+++ files/boot-unseal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/ash
 
-unlocked=0
+key_found=0
 # Loop through all devices to find the ESP
 for device in $(blkid -o device); do
     part_type=$(blkid -p $device -s PART_ENTRY_TYPE -o value)
@@ -12,23 +12,25 @@ for device in $(blkid -o device); do
     mkdir -p /mnt/esp
     mount -t vfat $device /mnt/esp
 
-    # Try all of the keys in the appropriate folder
+    # Attempt to unseal each sealed keyfile on the ESP. Note that just becasue
+    # the key is unsealed by the TPM, does not mean it will unlock the disk. We
+    # write the unsealed key to the in-memory rootfs, it is not written to disk.
     for f in /mnt/esp/*/disk_unlock_keys/*.sealed; do
-        if gotpm unseal --input "$f" --output "/crypto_keyfile.bin" ; then
+        if [ -f "$f" ] && gotpm unseal --input "$f" --output "/crypto_keyfile.bin" ; then
             echo "gotpm: Unsealed $f"
-            unlocked=1
+            key_found=1
             break
         fi
     done
     umount $device
 
-    if [ $unlocked -ne 0 ]; then
+    if [ $key_found -ne 0 ]; then
         break
     fi
 done
 
-if [ $unlocked -eq 0 ]; then
-    echo "gotpm: Unable to unseal TPM disk unlock key"
+if [ $key_found -eq 0 ]; then
+    echo "gotpm: Unable to unseal any TPM disk unlock key"
 fi
 
 # vim: set ft=sh ts=4 sw=4 et:
