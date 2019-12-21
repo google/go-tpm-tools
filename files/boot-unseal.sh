643f9542f3f4a1983a79a8da13fcee6f15280983
Joe Richey <joerichey@google.com>
2019-12-20 16:53:49 -0800

    boot-unseal: Don't prefix error messages

--- files/boot-unseal.sh
+++ files/boot-unseal.sh
@@ -16,21 +16,24 @@ for device in $(blkid -o device); do
     # the key is unsealed by the TPM, does not mean it will unlock the disk. We
     # write the unsealed key to the in-memory rootfs, it is not written to disk.
     for f in /mnt/esp/*/disk_unlock_keys/*.sealed; do
-        if [ -f "$f" ] && gotpm unseal --input "$f" --output "/crypto_keyfile.bin" ; then
-            echo "gotpm: Unsealed $f"
-            key_found=1
-            break
+        if [ -f "$f" ]; then
+            if gotpm unseal --input "$f" --output "/crypto_keyfile.bin" ; then
+                echo "Unsealed ${f#/mnt/esp}"
+                key_found=1
+                break
+            else
+                echo "Failed to unseal ${f#/mnt/esp}"
+            fi
         fi
     done
     umount $device
 
     if [ $key_found -ne 0 ]; then
-        break
+        exit 0
     fi
 done
 
-if [ $key_found -eq 0 ]; then
-    echo "gotpm: Unable to unseal any TPM disk unlock key"
-fi
+echo "Unable to unseal any TPM disk unlock key"
+exit 1
 
 # vim: set ft=sh ts=4 sw=4 et:
