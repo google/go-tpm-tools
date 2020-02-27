051e0addf079b0745db852f7937509a1000ad8bf
Joe Richey <joerichey@google.com>
2020-02-27 13:28:35 -0800

    internal: Only open the physical TPM once
    This prevents some flakes we've seen on Linux
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- internal/test_other.go
+++ internal/test_other.go
@@ -5,32 +5,18 @@ package internal
 import (
 	"flag"
 	"io"
-	"testing"
 
 	"github.com/google/go-tpm/tpm2"
-
-	"github.com/google/go-tpm-tools/simulator"
 )
 
 // As this package is only included in tests, this flag will not conflict with
 // the --tpm-path flag in gotpm/cmd
 var tpmPath = flag.String("tpm-path", "", "Path to Linux TPM character device (i.e. /dev/tpm0 or /dev/tpmrm0). Empty value (default) will run tests against the simulator.")
 
-// GetTPM is a cross-platform testing helper function that retrives the
-// appropriate TPM device from the flags passed into "go test".
-func GetTPM(tb testing.TB) io.ReadWriteCloser {
-	tb.Helper()
-	if *tpmPath != "" {
-		rwc, err := tpm2.OpenTPM(*tpmPath)
-		if err != nil {
-			tb.Fatalf("Opening TPM failed: %v", err)
-		}
-		return rwc
-	}
+func useRealTPM() bool {
+	return *tpmPath != ""
+}
 
-	simulator, err := simulator.Get()
-	if err != nil {
-		tb.Fatalf("Simulator initialization failed: %v", err)
-	}
-	return simulator
+func getRealTPM() (io.ReadWriteCloser, error) {
+	return tpm2.OpenTPM(*tpmPath)
 }
--- internal/test_windows.go
+++ internal/test_windows.go
@@ -3,30 +3,16 @@ package internal
 import (
 	"flag"
 	"io"
-	"testing"
 
 	"github.com/google/go-tpm/tpm2"
-
-	"github.com/google/go-tpm-tools/simulator"
 )
 
 var useTBS = flag.Bool("use-tbs", false, "Run the tests against the Windows TBS. Value of false (default) will run tests against the simulator.")
 
-// GetTPM is a cross-platform testing helper function that retrives the
-// appropriate TPM device from the flags passed into "go test".
-func GetTPM(tb testing.TB) io.ReadWriteCloser {
-	tb.Helper()
-	if *useTBS {
-		rwc, err := tpm2.OpenTPM()
-		if err != nil {
-			tb.Fatalf("Windows TBS initialization failed: %v", err)
-		}
-		return rwc
-	}
+func useRealTPM() bool {
+	return *useTBS
+}
 
-	simulator, err := simulator.Get()
-	if err != nil {
-		tb.Fatalf("Simulator initialization failed: %v", err)
-	}
-	return simulator
+func getRealTPM() (io.ReadWriteCloser, error) {
+	return tpm2.OpenTPM()
 }
