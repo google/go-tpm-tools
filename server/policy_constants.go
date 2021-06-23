d77fa72273908a10690f5d2d192efebaaa5bd7c2
Joe Richey <joerichey@google.com>
2021-06-23 04:40:05 -0700

    embed: Add certificates
    Signed-off-by: Joe Richey <joerichey@google.com>

---
+++ server/policy_constants.go
@@ -0,0 +1,25 @@
+package server
+
+import _ "embed"
+
+// Standard Secure Boot certificates (DER encoded)
+var (
+	//go:embed secure-boot/GcePk.crt
+	GceDefaultPKCert []byte
+	//go:embed secure-boot/MicCorKEKCA2011_2011-06-24.crt
+	MicrosoftKEKCA2011Cert []byte
+	//go:embed secure-boot/MicWinProPCA2011_2011-10-19.crt
+	WindowsProductionPCA2011Cert []byte
+	//go:embed secure-boot/MicCorUEFCA2011_2011-06-27.crt
+	MicrosoftUEFICA2011Cert []byte
+)
+
+// Revoked Signing certificates (DER encoded)
+var (
+	//go:embed secure-boot/canonical-boothole.crt
+	RevokedCanonicalBootholeCert []byte
+	//go:embed secure-boot/debian-boothole.crt
+	RevokedDebianBootholeCert []byte
+	//go:embed secure-boot/cisco-boothole.crt
+	RevokedCiscoCert []byte
+)
