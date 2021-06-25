d17259cd78d314e066f0cf7a3df95d2c751a451d
Joe Richey <joerichey@google.com>
2021-06-25 05:30:17 -0700

    proto: Add go:generate invocation
    Signed-off-by: Joe Richey <joerichey@google.com>

--- proto/doc.go
+++ proto/doc.go
@@ -1,3 +1,5 @@
 // Package proto contains protocol buffers that are exchanged between the client
 // and server. Note, some of these types have additional helper methods.
 package proto
+
+//go:generate protoc --go_out=. --go_opt=module=github.com/google/go-tpm-tools/proto tpm.proto
