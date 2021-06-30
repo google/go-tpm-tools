95a54f6bce2f0ac1ce97e3aa4287be4374db06e6
Joe Richey <joerichey@google.com>
2021-06-29 18:02:11 -0700

    server: Rename to GCEInstanceURL
    Signed-off-by: Joe Richey <joerichey@google.com>

--- server/instance_info.go
+++ server/instance_info.go
@@ -1,17 +1,19 @@
-package proto
+package server
 
 import (
 	"fmt"
 	"net/url"
+
+	pb "github.com/google/go-tpm-tools/proto"
 )
 
-// InstanceURL returns a Google API URL to the specified instance. This URL can
-// then be used with GCE instance APIs.
-func (x *GceInstanceInfo) InstanceURL() string {
+// GCEInstanceURL returns a Google API URL to the specified instance. This URL
+// can then be used with GCE instance APIs.
+func GCEInstanceURL(i *pb.GceInstanceInfo) string {
 	return fmt.Sprintf(
 		"https://www.googleapis.com/compute/v1/projects/%s/zones/%s/instances/%s",
-		url.PathEscape(x.GetProjectId()),
-		url.PathEscape(x.GetZone()),
-		url.PathEscape(x.GetInstanceName()), // Can use either the name or id here
+		url.PathEscape(i.GetProjectId()),
+		url.PathEscape(i.GetZone()),
+		url.PathEscape(i.GetInstanceName()), // Can use either the name or id here
 	)
 }
