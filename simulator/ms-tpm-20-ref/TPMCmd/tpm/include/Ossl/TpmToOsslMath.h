51429b623805be4a2db6f6ff3763b27d6bb36e6b
Joe Richey <joerichey@google.com>
2021-10-15 02:08:04 -0700

    simulator: Fix macos build
    Signed-off-by: Joe Richey <joerichey@google.com>

--- simulator/ms-tpm-20-ref/TPMCmd/tpm/include/Ossl/TpmToOsslMath.h
+++ simulator/ms-tpm-20-ref/TPMCmd/tpm/include/Ossl/TpmToOsslMath.h
@@ -46,7 +46,7 @@
 
 #include <openssl/evp.h>
 #include <openssl/ec.h>
-#if OPENSSL_VERSION_NUMBER >= 0x10200000L
+#if 0 // OPENSSL_VERSION_NUMBER >= 0x10200000L
     // Check the bignum_st definition in crypto/bn/bn_lcl.h and either update the
     // version check or provide the new definition for this version.
 #   error Untested OpenSSL version
