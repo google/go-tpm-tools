dddab70adb79004d861dbed1caa3b6c988aab06e
Alex Wu <wuale@google.com>
2022-11-18 09:44:38 -0800

    Remove gRPC client and use of insecure credentials (#262)
    Deprecate the use of the gRPC-based Attestation Verifier.
    This also removes the use of insecure credentials with gRPC.

--- launcher/agent/agent.go
+++ launcher/agent/agent.go
@@ -44,9 +44,7 @@ type agent struct {
 // attestation using the machine's (v)TPM to GCE's Attestation Service.
 // - tpm is a handle to the TPM on the instance
 // - akFetcher is a func to fetch an attestation key: see go-tpm-tools/client.
-// - conn is a client connection to the attestation service, typically created
-//
-//	`grpc.Dial`. It is the client's responsibility to close the connection.
+// - principalFetcher is a func to fetch GCE principal tokens for a given audience.
 func CreateAttestationAgent(tpm io.ReadWriteCloser, akFetcher tpmKeyFetcher, verifierClient verifier.Client, principalFetcher principalIDTokenFetcher) AttestationAgent {
 	return &agent{
 		tpm:              tpm,
