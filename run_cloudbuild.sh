b43c9922e45254ba64f22cda9c7d7f63659f94cd
Jiankun Lu <jiankun@google.com>
2022-12-02 15:58:13 -0800

    Update cloudbuild workflow for launcher
    Fix TextRefreshToken test.
    Create a overall cloudbuild file can trigger image building and
    testing cloudbuild workflow.
    
    Signed-off-by: Jiankun Lu <jiankun@google.com>

--- run_cloudbuild.sh
+++ run_cloudbuild.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-# Run script using run_cloudbuild.sh <image-type: debug, hardened>
-#
+# Run the script: ./run_cloudbuild.sh
 set -euxo pipefail
-
-if [ $# -eq 0 ]; then
-  echo "No arguments supplied. Run with image-type."
-  exit 1
-elif [[ "$1" != "hardened" && "$1" != "debug" ]]; then
-  echo "Incorrect args: image-type must be one of debug|hardened"
-  exit 1
-fi
 
 # Append a timestamp, as there is a check in finish-image-build that checks if
 # the image already exists.
-IMAGE_SUFFIX="$1-$USER-test-image-`date +%s`"
-BUCKET_NAME="$USER-confidential-space-test-images"
+IMAGE_SUFFIX="$USER-test-image-`date +%s`"
 
 DIR=$(dirname -- "${BASH_SOURCE[0]}")
 echo "Running Cloud Build on directory $DIR"
@@ -24,9 +14,10 @@ echo "Running Cloud Build on directory $DIR"
 #
 # Ensure you grant Cloud Build access to Compute Images:
 # https://pantheon.corp.google.com/compute/images?referrer=search&tab=exports&project=$PROJECT_ID
-gcloud beta builds submit --config=$DIR/cloudbuild.yaml \
-  --substitutions=_OUTPUT_IMAGE_SUFFIX="$IMAGE_SUFFIX",_BUCKET_NAME="$BUCKET_NAME",_IMAGE_ENV="$1"
+gcloud beta builds submit --config=${DIR}/cloudbuild.yaml \
+  --substitutions=_OUTPUT_IMAGE_SUFFIX="${IMAGE_SUFFIX}"
 
 echo "Image creation successful."
-echo "Create a VM using:"
-echo "gcloud compute instances create confidential-space-test --image=confidential-space-$IMAGE_SUFFIX"
+echo "Create a VM using the debug image confidential-space-debug-${IMAGE_SUFFIX}"
+echo "gcloud compute instances create confidential-space-test --image=confidential-space-debug-${IMAGE_SUFFIX} --metadata ..."
+echo "Or use the hardened image confidential-space-hardened-${IMAGE_SUFFIX}"
