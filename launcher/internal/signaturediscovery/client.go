a534ad4e0a60e211bf050b46f491517a848d9966
yawangwang <yawangwang@google.com>
2023-07-14 20:42:16 +0000

    [Signature discovery] Fetch a signed image manifest for parsing (#324)
    * Adding signature discovery to fetch signed image manifest
    
    * Fix CI lint issues
    
    * Rename signature-discovery/client to intenral/signaturediscovery/client

---
+++ launcher/internal/signaturediscovery/client.go
@@ -0,0 +1,63 @@
+// Package signaturediscovery contains functionalities to discover container image signatures.
+package signaturediscovery
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/images"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+const signatureTagSuffix = "sig"
+
+// Client is a wrapper of containerd.Client to interact with signed image manifest.
+type Client struct {
+	cdClient          *containerd.Client
+	OriginalImageDesc v1.Descriptor
+	RemoteOpts        []containerd.RemoteOpt
+}
+
+// New creates a new client.
+func New(cdClient *containerd.Client, originalImageDesc v1.Descriptor, opts ...containerd.RemoteOpt) *Client {
+	return &Client{
+		cdClient:          cdClient,
+		OriginalImageDesc: originalImageDesc,
+		RemoteOpts:        opts,
+	}
+}
+
+// FetchSignedImageManifest fetches a signed image manifest using a tag-based discovery mechanism.
+func (c *Client) FetchSignedImageManifest(ctx context.Context, targetRepository string) (v1.Manifest, error) {
+	image, err := c.pullTargetImage(ctx, targetRepository)
+	if err != nil {
+		return v1.Manifest{}, err
+	}
+	return getManifest(ctx, image)
+}
+
+func (c *Client) pullTargetImage(ctx context.Context, targetRepository string) (containerd.Image, error) {
+	targetImageRef := fmt.Sprint(targetRepository, ":", formatSigTag(c.OriginalImageDesc))
+	image, err := c.cdClient.Pull(ctx, targetImageRef, c.RemoteOpts...)
+	if err != nil {
+		return nil, fmt.Errorf("cannot pull the signature object [%s] from target repository [%s]: %w", targetImageRef, targetRepository, err)
+	}
+	return image, nil
+}
+
+// formatSigTag turns image digests into tags with signatureTagSuffix:
+// sha256:9ecc53c2 -> sha256-9ecc53c2.sig
+func formatSigTag(imageDesc v1.Descriptor) string {
+	digest := imageDesc.Digest
+	return fmt.Sprint(digest.Algorithm(), "-", digest.Encoded(), ".", signatureTagSuffix)
+}
+
+func getManifest(ctx context.Context, image containerd.Image) (v1.Manifest, error) {
+	cs := image.ContentStore()
+	manifest, err := images.Manifest(ctx, cs, image.Target(), image.Platform())
+	if err != nil {
+		return v1.Manifest{}, err
+	}
+	return manifest, nil
+}
---
+++ launcher/internal/signaturediscovery/client_test.go
@@ -0,0 +1,64 @@
+package signaturediscovery
+
+import (
+	"context"
+	"testing"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/defaults"
+	"github.com/containerd/containerd/namespaces"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+func TestFormatSigTag(t *testing.T) {
+	testCases := []struct {
+		name       string
+		imageDesc  v1.Descriptor
+		wantSigTag string
+		wantPass   bool
+	}{
+		{
+			name:       "formatSigTag success",
+			imageDesc:  v1.Descriptor{Digest: "sha256:9ecc53c269509f63c69a266168e4a687c7eb8c0cfd753bd8bfcaa4f58a90876f"},
+			wantSigTag: "sha256-9ecc53c269509f63c69a266168e4a687c7eb8c0cfd753bd8bfcaa4f58a90876f.sig",
+			wantPass:   true,
+		},
+		{
+			name:       "formatSigTag failed with wrong image digest",
+			imageDesc:  v1.Descriptor{Digest: "sha256:9ecc53c269509f63c69a266168e4a687c7eb8c0cfd753bd8bfcaa4f58a90876f"},
+			wantSigTag: "sha256-18740b995b4eac1b5706392a96ff8c4f30cefac18772058a71449692f1581f0f.sig",
+			wantPass:   false,
+		},
+		{
+			name:       "formatSigTag failed with wrong tag format",
+			imageDesc:  v1.Descriptor{Digest: "sha256:9ecc53c269509f63c69a266168e4a687c7eb8c0cfd753bd8bfcaa4f58a90876f"},
+			wantSigTag: "sha256@9ecc53c269509f63c69a266168e4a687c7eb8c0cfd753bd8bfcaa4f58a90876f.sig",
+			wantPass:   false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := formatSigTag(tc.imageDesc) == tc.wantSigTag; got != tc.wantPass {
+				t.Errorf("formatSigTag() failed for test case %v: got %v, wantPass %v", tc.name, got, tc.wantPass)
+			}
+		})
+	}
+}
+
+func TestFetchSignedImageManifestDockerPublic(t *testing.T) {
+	ctx := namespaces.WithNamespace(context.Background(), "test")
+	containerdClient, err := containerd.New(defaults.DefaultAddress)
+	if err != nil {
+		t.Skipf("test needs containerd daemon: %v", err)
+	}
+	defer containerdClient.Close()
+
+	targetRepository := "gcr.io/distroless/static"
+	originalImageDesc := v1.Descriptor{Digest: "sha256:9ecc53c269509f63c69a266168e4a687c7eb8c0cfd753bd8bfcaa4f58a90876f"}
+	client := New(containerdClient, originalImageDesc)
+	// testing image manifest fetching using a public docker repo url
+	if _, err := client.FetchSignedImageManifest(ctx, targetRepository); err != nil {
+		t.Errorf("failed to fetch signed image manifest from targetRepository [%s]: %v", targetRepository, err)
+	}
+}
