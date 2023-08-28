884b9410407c66bbfba81c5817eedacb05163a1b
Pranjali-2501 <87357388+Pranjali-2501@users.noreply.github.com>
2023-08-28 17:27:57 +0000

    Integration of go-tpm-tools with go-tdx-guest (#347)
    When getting a vTPM quote within a TDX VM, the attestation service will utilize go-tdx-guest to generate and verify Intel's TDX hardware based attestation quote.
    
    Add TDX attestation protobuf in the go-tpm-tools attestation message.
    Update client changes for TDX device-related attestation quote generation.
    Update server changes for TDX quote verification using go-tdx-guest.
    Enhance CLI tool for TDX functionality updates.
    
    * Integration of go-tpm-tools with go-tdx-guest
    
    * modify tdx/sev-snp tests in cmd
    
    * add checks to verify TEEopts in case of NONE, SEV and SEV_ES
    
    * change protoc-gen-go version
    
    * update go-tdx-guest version
    
    * modify protobuf version
    
    ---------
    
    Co-authored-by: Pranjali Saxena <pranjalisaxena@google.com>

--- cmd/verify.go
+++ cmd/verify.go
@@ -6,6 +6,7 @@ import (
 	"io"
 
 	sv "github.com/google/go-sev-guest/verify"
+	tv "github.com/google/go-tdx-guest/verify"
 	pb "github.com/google/go-tpm-tools/proto/attest"
 	"github.com/google/go-tpm-tools/server"
 	"github.com/google/go-tpm/legacy/tpm2"
@@ -47,17 +48,27 @@ var debugCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		var validateOpts *server.VerifySnpOpts
-		if len(teeNonce) != 0 {
-			validateOpts = &server.VerifySnpOpts{
-				Validation:   server.SevSnpDefaultValidateOpts(teeNonce),
-				Verification: &sv.Options{},
+
+		var validateOpts interface{}
+		switch attestation.GetTeeAttestation().(type) {
+		case *pb.Attestation_TdxAttestation:
+			validateOpts = &server.VerifyTdxOpts{
+				Verification: tv.DefaultOptions(),
 			}
-		} else {
-			validateOpts = &server.VerifySnpOpts{
-				Validation:   server.SevSnpDefaultValidateOpts(nonce),
-				Verification: &sv.Options{},
+		case *pb.Attestation_SevSnpAttestation:
+			if len(teeNonce) != 0 {
+				validateOpts = &server.VerifySnpOpts{
+					Validation:   server.SevSnpDefaultValidateOpts(teeNonce),
+					Verification: &sv.Options{},
+				}
+			} else {
+				validateOpts = &server.VerifySnpOpts{
+					Validation:   server.SevSnpDefaultValidateOpts(nonce),
+					Verification: &sv.Options{},
+				}
 			}
+		default:
+			validateOpts = nil
 		}
 		ms, err := server.VerifyAttestation(attestation, server.VerifyOpts{Nonce: nonce, TrustedAKs: []crypto.PublicKey{cryptoPub}, TEEOpts: validateOpts})
 		if err != nil {
