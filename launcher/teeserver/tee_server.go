09e66df176b209bdb5006cd5112cdfa44276d3eb
Sibghat Shah <sibghat@google.com>
2025-07-15 22:55:27 +0000

    TestCustomToken now lets empty audience through

--- launcher/teeserver/tee_server.go
+++ launcher/teeserver/tee_server.go
@@ -165,6 +165,7 @@ func (a *attestHandler) attest(w http.ResponseWriter, r *http.Request, client ve
 		if tokenOptions.Audience == "" {
 
 			tokenOptions.Audience = audienceSTS
+			// return
 		}
 
 		if tokenOptions.TokenType == "" {
