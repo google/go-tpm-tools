00e829497e6662e510c5dd338665c11fd5a1ea6a
Jessie Liu <jessieqliu@google.com>
2025-09-02 18:42:19 +0000

    add comment for GetTPM

--- testutil/utils.go
+++ testutil/utils.go
@@ -8,6 +8,11 @@ import (
 	"github.com/google/go-tpm-tools/internal/test"
 )
 
+// GetTPM is a cross-platform testing helper function that retrives the
+// appropriate TPM device from the flags passed into "go test".
+//
+// If using a test TPM, this will also retrieve a test eventlog. In this case,
+// GetTPM extends the test event log's events into the test TPM.
 func GetTPM(tb testing.TB) io.ReadWriteCloser {
 	return test.GetTPM(tb)
 }
