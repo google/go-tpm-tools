43d8e7ae0cfc15d1cd0da1edd3c7d9dafc4ee592
Nilanjan Daw <nilanjandaw@google.com>
2026-03-02 07:41:40 +0000

    32-bit lint error

--- keymanager/workload_service/key_custody_core/ws_key_custody_core_stub.go
+++ keymanager/workload_service/key_custody_core/ws_key_custody_core_stub.go
@@ -7,15 +7,15 @@ import (
 
 	"github.com/google/uuid"
 
-	algorithms "github.com/google/go-tpm-tools/keymanager/km_common/proto"
+	keymanager "github.com/google/go-tpm-tools/keymanager/km_common/proto"
 )
 
 // GenerateBindingKeypair is a stub for architectures where the Rust library is not supported.
-func GenerateBindingKeypair(_ *algorithms.HpkeAlgorithm, _ uint64) (uuid.UUID, []byte, error) {
+func GenerateBindingKeypair(_ *keymanager.HpkeAlgorithm, _ uint64) (uuid.UUID, []byte, error) {
 	return uuid.Nil, nil, fmt.Errorf("GenerateBindingKeypair is not supported on this architecture")
 }
 
 // GetBindingKey is a stub for architectures where the Rust library is not supported.
-func GetBindingKey(id uuid.UUID) ([]byte, error) {
-	return nil, fmt.Errorf("GetBindingKey is not supported on this architecture")
+func GetBindingKey(id uuid.UUID) ([]byte, *keymanager.HpkeAlgorithm, error) {
+	return nil, nil, fmt.Errorf("GetBindingKey is not supported on this architecture")
 }
