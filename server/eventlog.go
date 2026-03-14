a05ea13219e6db2be21939ac0d7de34c57858acb
Alex Wu <wuale@google.com>
2026-03-14 00:01:21 +0000

    Remove ParseCosCELPCR and ParseCosCELRTMR
    Removing these funcs in favor of the go-eventlog cel package. We still
    need to remove the cel package in go-tpm-tools, and that will come in a
    later PR.
    
    This is a breaking change.

--- server/eventlog.go
+++ server/eventlog.go
@@ -1,21 +1,15 @@
 package server
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 
-	attestationpb "github.com/GoogleCloudPlatform/confidential-space/server/proto/gen/attestation"
-
-	gecel "github.com/google/go-eventlog/cel"
 	"github.com/google/go-eventlog/extract"
 	gepb "github.com/google/go-eventlog/proto/state"
 	"github.com/google/go-eventlog/register"
 	"github.com/google/go-eventlog/tpmeventlog"
-	"github.com/google/go-tpm-tools/cel"
 	pb "github.com/google/go-tpm-tools/proto/attest"
 	tpmpb "github.com/google/go-tpm-tools/proto/tpm"
-	"google.golang.org/protobuf/proto"
 )
 
 // parsePCClientEventLog parses a raw event log and replays the parsed event
@@ -311,159 +305,6 @@ func convertFromPbDatabase(pbdb *pb.Database) *gepb.Database {
 		Hashes: pbdb.GetHashes(),
 	}
 
-}
-
-// ParseCosCELPCR takes an encoded COS CEL and PCR bank, replays the CEL against the PCRs,
-// and returns the AttestedCosState
-func ParseCosCELPCR(cosEventLog []byte, p register.PCRBank) (*pb.AttestedCosState, error) {
-	return getCosStateFromCEL(cosEventLog, p, gecel.PCRType)
-}
-
-// ParseCosCELRTMR takes in a raw COS CEL and a RTMR bank, validates and returns it's
-// COS states as parts of the MachineState.
-func ParseCosCELRTMR(cosEventLog []byte, r register.RTMRBank) (*pb.AttestedCosState, error) {
-	return getCosStateFromCEL(cosEventLog, r, gecel.CCMRType)
-}
-
-func getCosStateFromCEL(rawCanonicalEventLog []byte, register register.MRBank, trustingRegisterType gecel.MRType) (*pb.AttestedCosState, error) {
-	decodedCEL, err := gecel.DecodeToCEL(bytes.NewBuffer(rawCanonicalEventLog))
-	if err != nil {
-		return nil, err
-	}
-	// Validate the COS event log first.
-	if err := decodedCEL.Replay(register); err != nil {
-		return nil, err
-	}
-
-	cosState, err := getVerifiedCosState(decodedCEL, trustingRegisterType)
-	if err != nil {
-		return nil, err
-	}
-
-	return cosState, err
-}
-
-// getVerifiedCosState takes in CEL and a register type (can be PCR or CCELMR), and returns the state
-// in the CEL. It will only include events using the correct registerType.
-func getVerifiedCosState(coscel gecel.CEL, registerType gecel.MRType) (*pb.AttestedCosState, error) {
-	cosState := &pb.AttestedCosState{}
-	cosState.Container = &pb.ContainerState{}
-	cosState.HealthMonitoring = &pb.HealthMonitoringState{}
-	cosState.GpuDeviceState = &pb.GpuDeviceState{}
-	cosState.Container.Args = make([]string, 0)
-	cosState.Container.EnvVars = make(map[string]string)
-	cosState.Container.OverriddenEnvVars = make(map[string]string)
-
-	seenSeparator := false
-	for _, record := range coscel.Records() {
-		if record.IndexType != registerType {
-			return nil, fmt.Errorf("expect registerType: %d, but get %d in a CEL record", registerType, record.IndexType)
-		}
-
-		switch record.IndexType {
-		case gecel.PCRType:
-			if record.Index != cel.CosEventPCR {
-				return nil, fmt.Errorf("found unexpected PCR %d in COS CEL log", record.Index)
-			}
-		case gecel.CCMRType:
-			if record.Index != cel.CosCCELMRIndex {
-				return nil, fmt.Errorf("found unexpected CCELMR %d in COS CEL log", record.Index)
-			}
-		default:
-			return nil, fmt.Errorf("unknown COS CEL log index type %d", record.IndexType)
-		}
-
-		// The Content.Type is not verified at this point, so we have to fail
-		// if we see any events that we do not understand. This ensures that
-		// we either verify the digest of event event in this PCR, or we fail
-		// to replay the event log.
-		// TODO: See if we can fix this to have the Content Type be verified.
-		cosTlv, err := cel.ParseToCosTlv(record.Content)
-		if err != nil {
-			return nil, err
-		}
-
-		// verify digests for the cos cel content
-		if err := gecel.VerifyDigests(cosTlv, record.Digests); err != nil {
-			return nil, err
-		}
-
-		// TODO: Add support for post-separator container data
-		if seenSeparator {
-			return nil, fmt.Errorf("found COS Event Type %v after LaunchSeparator event", cosTlv.EventType)
-		}
-
-		switch cosTlv.EventType {
-		case cel.ImageRefType:
-			if cosState.Container.GetImageReference() != "" {
-				return nil, fmt.Errorf("found more than one ImageRef event")
-			}
-			cosState.Container.ImageReference = string(cosTlv.EventContent)
-
-		case cel.ImageDigestType:
-			if cosState.Container.GetImageDigest() != "" {
-				return nil, fmt.Errorf("found more than one ImageDigest event")
-			}
-			cosState.Container.ImageDigest = string(cosTlv.EventContent)
-
-		case cel.RestartPolicyType:
-			restartPolicy, ok := pb.RestartPolicy_value[string(cosTlv.EventContent)]
-			if !ok {
-				return nil, fmt.Errorf("unknown restart policy in COS eventlog: %s", string(cosTlv.EventContent))
-			}
-			cosState.Container.RestartPolicy = pb.RestartPolicy(restartPolicy)
-
-		case cel.ImageIDType:
-			if cosState.Container.GetImageId() != "" {
-				return nil, fmt.Errorf("found more than one ImageId event")
-			}
-			cosState.Container.ImageId = string(cosTlv.EventContent)
-
-		case cel.EnvVarType:
-			envName, envVal, err := cel.ParseEnvVar(string(cosTlv.EventContent))
-			if err != nil {
-				return nil, err
-			}
-			cosState.Container.EnvVars[envName] = envVal
-
-		case cel.ArgType:
-			cosState.Container.Args = append(cosState.Container.Args, string(cosTlv.EventContent))
-
-		case cel.OverrideArgType:
-			cosState.Container.OverriddenArgs = append(cosState.Container.OverriddenArgs, string(cosTlv.EventContent))
-
-		case cel.OverrideEnvType:
-			envName, envVal, err := cel.ParseEnvVar(string(cosTlv.EventContent))
-			if err != nil {
-				return nil, err
-			}
-			cosState.Container.OverriddenEnvVars[envName] = envVal
-		case cel.LaunchSeparatorType:
-			seenSeparator = true
-		case cel.MemoryMonitorType:
-			enabled := false
-			if len(cosTlv.EventContent) == 1 && cosTlv.EventContent[0] == uint8(1) {
-				enabled = true
-			}
-			cosState.HealthMonitoring.MemoryEnabled = &enabled
-		case cel.GpuCCModeType:
-			ccMode, ok := pb.GPUDeviceCCMode_value[string(cosTlv.EventContent)]
-			if !ok {
-				return nil, fmt.Errorf("unknown GPU device CC mode in COS eventlog: %s", string(cosTlv.EventContent))
-			}
-			cosState.GpuDeviceState.CcMode = pb.GPUDeviceCCMode(ccMode)
-		case cel.GPUDeviceAttestationBindingType:
-			report := &attestationpb.NvidiaAttestationReport{}
-			if err := proto.Unmarshal(cosTlv.EventContent, report); err != nil {
-				return nil, fmt.Errorf("failed to unmarshal GPU attestation report: %v", err)
-			}
-
-		default:
-			return nil, fmt.Errorf("found unknown COS Event Type %v", cosTlv.EventType)
-		}
-
-	}
-	return cosState, nil
 }
 
 func convertToPbDatabase(gedb *gepb.Database) *pb.Database {
