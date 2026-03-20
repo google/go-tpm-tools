cda095f529c2f0b1fa70923caa9c9b444aaf072c
Nilanjan Daw <nilanjandaw@google.com>
2026-03-20 08:05:41 +0000

    add error.Is and Error string commands

--- keymanager/km_common/proto/errors.go
+++ keymanager/km_common/proto/errors.go
@@ -14,6 +14,22 @@ func (e *FFIError) Error() string {
 	return fmt.Sprintf("FFI error: %s", e.Code.String())
 }
 
+// Is allows users to check errors.Is(err, Error_CODE) or errors.Is(err, &FFIError{Code: Error_CODE}).
+func (e *FFIError) Is(target error) bool {
+	if t, ok := target.(Error); ok {
+		return e.Code == t
+	}
+	if t, ok := target.(*FFIError); ok {
+		return e.Code == t.Code
+	}
+	return false
+}
+
+// Error allows Error to be used as a Go error.
+func (e Error) Error() string {
+	return e.String()
+}
+
 // ToError converts an Error to a Go error, or returns nil if it is Success.
 func (e Error) ToError() error {
 	if e == Error_ERROR_SUCCESS {
