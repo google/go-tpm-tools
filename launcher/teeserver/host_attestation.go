f1bb586f50409d44c27786fa6cfc6f65728a691a
mayafleischer <mfleischer@google.com>
2026-04-10 20:43:33 +0000

    update to use binarypb

--- launcher/teeserver/host_attestation.go
+++ launcher/teeserver/host_attestation.go
@@ -1,41 +1,22 @@
 package teeserver
 
 import (
-	"github.com/GoogleCloudPlatform/confidential-space/server/labels"
+	"encoding/base64"
+
 	attestationpb "github.com/GoogleCloudPlatform/confidential-space/server/proto/gen/attestation"
+	"google.golang.org/protobuf/proto"
 )
 
-func dummyHostAttestation(challenge []byte) *attestationpb.HostAttestation {
-	return &attestationpb.HostAttestation{
-		Label:     []byte(labels.HostAttestation),
-		Challenge: challenge,
-		ExtraData: []byte("dummy-extra-data"),
-		TpmQuote: &attestationpb.TpmQuote{
-			Quotes: []*attestationpb.TpmQuote_SignedQuote{
-				{
-					HashAlgorithm: 11, // TPM_ALG_SHA256
-					PcrValues: map[uint32][]byte{
-						0:  []byte("/%\235\307QWC\241\004\362\0133\350a\202X\362\n\034\275\001\343}\250\347!2\342F\243\306\234"),
-						17: []byte("\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"),
-						18: []byte("\357\2068\010@\307\323i\366){\227\253h\334\240$:\375Q\252G\264\275`>,\0303\335\033B"),
-						21: []byte("\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"),
-					},
-					TpmsAttest:    []byte("\377TCG\200\030\000\"\000\013\347`Cr-K\321\226\373\341\270S\355\243\306\027=l\321\273\212\255\233\301\355\300\354$\211^.\224\000 ]\033`\314.\001E\247\305\224\245\224\004u\242)\330\267\346\332\215\346\244\027Vx6\350c\377\246z\000\000\000\000\t\326G\350\000\000\000\001\000\000\000\000\000\000\000\000\006x\302\244\027\000\000\000\001\000\013\003\001\000&\000 \272\252\325\271\326$!Q\334\021;\362\267n\377\240\346\204a\212q\234E\344CJ\002O|vN\031"),
-					TpmtSignature: []byte("\000\030\000\013\000 sY\303\255!iV\033\331S\346e\254ns\360c\322\226\200\373^)\231\223\204A\212\356)\000N\000 r\211\231\265M\365\376N\230d\264\025 2\232\375\364\331\277}n\364#\332YI@\222\n\016\342\303"),
-				},
-			},
-			PcclientBootEventLog: []byte("dummy_pcclient_event_log"),
-			CelLaunchEventLog:    []byte("dummy_cel_launch_event_log"),
-		},
-		AuxAttestation: &attestationpb.TpmAuxiliaryAttestation{
-			SignedNvs: []*attestationpb.TpmAuxiliaryAttestation_SignedNvCertify{
-				{
-					NvData:        []byte("\000\000\000\000\000\000\000\001"),
-					TpmsNvPublic:  []byte("\001\301\000\004\000\013\"\002\000\022\000\000\000\010"),
-					TpmsAttest:    []byte("\377TCG\200\024\000\"\000\013\347`Cr-K\321\226\373\341\270S\355\243\306\027=l\321\273\212\255\233\301\355\300\354$\211^.\224\000 ]\033`\314.\001E\247\305\224\245\224\004u\242)\330\267\346\332\215\346\244\027Vx6\350c\377\246z\000\000\000\000\t\326J\375\000\000\000\001\000\000\000\000\000\000\000\000\006x\302\244\027\000\"\000\013,C\204\263\274(\371\350\213\024\034\215\376\323\305\364\023]\343\021\302\004\204r\325\017+)\340\000\217+\000\000\000\010\000\000\000\000\000\000\000\001"),
-					TpmtSignature: []byte("\000\030\000\013\000 q\327\330\221\262a\361\277\302A\211\275s`\005\005\324\035\362-<@\017\002B\017\356r\367E\322_\000 \036\304r\232\371kiP\371~\301\020\275\2605P/w;,\204\035\212u\353\223H\016}yY\254"),
-				},
-			},
-		},
+const hostAttestationTemplateBinaryPBBase64 = "Kv8BCvwBEggAAAAAAAAAARoOAcEABAALIgIAEgAAAAgilQH/VENHgBQAIgAL52BDci1L0Zb74bhT7aPGFz1s0buKrZvB7cDsJIleLpQAIF0bYMwuAUWnxZSllAR1oinYt+bajeakF1Z4Nuhj/6Z6AAAAAAnWSv0AAAABAAAAAAAAAAAGeMKkFwAiAAssQ4SzvCj56IsUHI3+08X0E13jEcIEhHLVDysp4ACPKwAAAAgAAAAAAAAAASpIABgACwAgcdfYkbJh8b/CQYm9c2AFBdQd8i08QA8CQg/ucvdF0l8AIB7Ecpr5a2lQ+X7BEL2wNVAvdzsshB2KdeuTSA59eVmsIt0JCvgCCAsSJAgREiD//////////////////////////////////////////xIkCBISIO+GOAhAx9Np9il7l6to3KAkOv1Rqke0vWA+LBgz3RtCEiQIFRIg//////////////////////////////////////////8SJAgAEiAvJZ3HUVdDoQTyCzPoYYJY8gocvQHjfajnITLiRqPGnBqRAf9UQ0eAGAAiAAvnYENyLUvRlvvhuFPto8YXPWzRu4qtm8HtwOwkiV4ulAAgXRtgzC4BRafFlKWUBHWiKdi35tqN5qQXVng26GP/pnoAAAAACdZH6AAAAAEAAAAAAAAAAAZ4wqQXAAAAAQALAwEAJgAguqrVudYkIVHcETvyt27/oOaEYYpxnEXkQ0oCT3x2ThkiSAAYAAsAIHNZw60haVYb2VPmZaxuc/Bj0paA+14pmZOEQYruKQBOACByiZm1TfX+TphktBUgMpr99Nm/fW70I9pZSUCSCg7iwxIYZHVtbXlfcGNjbGllbnRfZXZlbnRfbG9nGhpkdW1teV9jZWxfbGF1bmNoX2V2ZW50X2xvZyKpBhKmBgqAAgEAAAABAAAAAAgAAJ9AGH46p1X5AAAAAAAAAAAAAAAAOkCBBSc3qVUEAAAABAKXBAAAAAAGAEAAAAAAAAAAAAD0JfNo3Bu+LmjsHL/LhEMHP1XTkXCRPBr8wAkB3KrkSOdgQ3ItS9GW++G4U+2jxhc9bNG7iq2bwe3A7CSJXi6UfaS7pgXkJOn4j3uGalz63UsYF2D228E7nIlPfKGGetSAFxbCQF2jU1zQBd7dkxYRfMwBzzVb2aW+/2NiJgb+twDfO1NhLvnQnyS+GKn8dn7Sws4xifyTpsSnbFQja7mW0t7yd7DKxZtN0tGsNqt3ltbhZpcCPLycY4z8l1eaXv8SoAQBAAAACwAAAAAGAABmrbvBRyJNxgAAAAAAAAAAAAAAADpAgQUnN6lVBAAAAAQClwQAAAAABgBAAAAAAAAAAAAA4++yicHdAIpeosTh6ar0RrgSBX8RcIi7HOAPseoaFrc/Y4Hoz8foJD4YdthDterYJcJWnRRpRA1ggf8n7UDyyZSanntAHE277l80odqD4YJUbQEF9B2Y5YpQt2Y4gWAwo5YRngxUtGCwZz7u5YOAfAfWvaypyxBL0jOYVL+zjHYBAAAACQAAAAAFAAAMYNgxY1BQUOvpHGD/P93lAAAAAAxg2DEQ3eXULTZa5jnH4fo6QIEFJzepVQQAgAAEApcEbA/MVFvLj4zaBYSnyJpD2URn3ciyHrSI9CtepCZ1uBMnTTBHLc0HNXlhXEhtAALRYggcVshhL6vRkNRh2hrrwE9KGYHASac0+jPdfxkGzSQBZd4bJl3xNAaT+aK6Xwm4N2c3Q90WUysSAAem7H0svKHXdmK9rWQpcKer6byWNcv3hd8TGQZCmB28vSOWwITHLqBeLn8QCu53p3SYd/JfmPQsd1XiRjNhx3b1mzuuSPyMjguFSt/FKxJ7WEQQIhPixfxxwkNxepTxoz6fgBuMeJKdAeMNZoWStI4nOuk93S8Y64cVJpRCPMjna2cdegxAQnKX1ypujCZZIv1B8oUHQomhp9fF3WX/Hz1afff3QwHTAytmXm6aIXiKeJK1bIHPEiCrq6urq6urq6urq6urq6urq6urq6urq6urq6urq6urqwoQSE9TVF9BVFRFU1RBVElPTg=="
+
+func dummyHostAttestation(_ []byte) *attestationpb.HostAttestation {
+	evidence := &attestationpb.HostAttestation{}
+	b, err := base64.StdEncoding.DecodeString(hostAttestationTemplateBinaryPBBase64)
+	if err != nil {
+		panic("failed to decode host attestation template binarypb: " + err.Error())
 	}
+	if err := proto.Unmarshal(b, evidence); err != nil {
+		panic("failed to unmarshal host attestation template: " + err.Error())
+	}
+	return evidence
 }
