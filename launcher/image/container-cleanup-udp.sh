49e55f63bad22c9a85cd85740fbb56c61e634dee
Min Hong Yun <mhyun@google.com>
2026-04-10 17:10:44 -0700

    Make the clean service wanted by cloud-init

--- launcher/image/container-cleanup-udp.sh
+++ launcher/image/container-cleanup-udp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# This script listens on UDP port 2080, executes received commands in bash,
-# and returns the output (stdout and stderr) to the sender.
 
-echo "Starting UDP listener on port 2080..."
+while true; do
+  # Capture the status in a variable
+  STATUS=$(systemctl status cloud-final.service)
 
-# UDP4-LISTEN:2080 -> Listens on UDP port 2080
-# fork -> Creates a child process for each request so the main listener stays alive
-# EXEC:"/bin/bash" -> Runs the received data as a command in bash
-# stderr -> Redirects standard error to the network socket so you see errors too
+  # Pipe the heartbeat and status into socat
+  echo "--- HEARTBEAT: $(date) ---
+$STATUS" | socat - UDP4:10.138.0.10:2020
 
-socat UDP4-LISTEN:2080,fork EXEC:"/bin/bash -c 'bash 2>&1 | tee /dev/ttyS0'"
+  sleep 1
+done
