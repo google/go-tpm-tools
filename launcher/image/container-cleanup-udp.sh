5aca72a76b2af0ff5d6c71b112469fcd8f515b61
Min Hong Yun <mhyun@google.com>
2026-04-11 11:43:57 -0700

    Trap SIGTERM from heartbeat

--- launcher/image/container-cleanup-udp.sh
+++ launcher/image/container-cleanup-udp.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Function to run when the script receives SIGTERM (shutdown)
+cleanup() {
+  echo "Caught SIGTERM! Running cleanup script..." | tee /dev/ttyS0 | socat - UDP4:10.138.0.10:2020
+
+  # Call your actual cleanup script here
+  /usr/share/oem/confidential_space/container-cleanup.sh
+
+  exit 0
+}
+
+# Trap SIGTERM and call the cleanup function
+trap 'cleanup' SIGTERM
+
 while true; do
   # Capture the status and dependencies in variables
   STATUS=$(systemctl status cloud-final.service)
