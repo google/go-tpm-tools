fa85c51ce112d887c684670bf3c6fb5ffa8e5973
Min Hong Yun <mhyun@google.com>
2026-04-11 08:01:18 -0700

    ExecStart script shows deps

--- launcher/image/container-cleanup-udp.sh
+++ launcher/image/container-cleanup-udp.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 while true; do
-  # Capture the status in a variable
+  # Capture the status and dependencies in variables
   STATUS=$(systemctl status cloud-final.service)
+  DEPS=$(systemctl list-dependencies cloud-final.service)
 
-  # Pipe the heartbeat and status into socat
+  # Pipe everything into socat
   echo "--- HEARTBEAT: $(date) ---
-$STATUS" | socat - UDP4:10.138.0.10:2020
+[STATUS]
+$STATUS
 
-  sleep 1
+[DEPENDENCIES]
+$DEPS" | socat - UDP4:10.138.0.10:2020
+
+  sleep 5
 done
