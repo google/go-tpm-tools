18815934462b46bb33bc38e54674d42642c193f7
jacobdw <jacobdw@google.com>
2026-05-12 15:16:15 -0700

    Adding network setup script to prioritize idpf nics and provide tap dev IP address
    Refactor network setup script into a dedicated static file
    
    remove accidental new line
    
    move network script to bc guest directory

---
+++ launcher/image/bc-guest/bc_network_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+mkdir -p /etc/systemd/network/
+
+cat << 'EOF' > /etc/systemd/network/10-idpf.network
+[Match]
+Driver=idpf
+
+[Network]
+DHCP=yes
+IPv6AcceptRA=yes
+
+[DHCPv4]
+RouteMetric=100
+
+[DHCPv6]
+RouteMetric=100
+EOF
+
+cat << 'EOF' > /etc/systemd/network/10-virtio.network
+[Match]
+Driver=virtio_net
+
+[Network]
+Address=192.168.100.2/24
+
+[Route]
+Gateway=192.168.100.1
+Metric=1024
+EOF
