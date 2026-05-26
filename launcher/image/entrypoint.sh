e39ac677b2fded3d6c42711f4c46b1ddbdf2f638
Min Hong Yun <mhyun@google.com>
2026-05-26 14:45:17 -0700

    Add graceful shutdown tests for a debug image.
    Also, deprive logind of its power button handling capability for debug
    images to ensure the same behavior as a hardened image.

--- launcher/image/entrypoint.sh
+++ launcher/image/entrypoint.sh
@@ -5,6 +5,12 @@ main() {
   cp /usr/share/oem/confidential_space/container-runner.service /etc/systemd/system/container-runner.service
   # Override default fluent-bit config.
   cp /usr/share/oem/confidential_space/fluent-bit-cs.conf /etc/fluent-bit/fluent-bit.conf
+  # Deprive logind of its power button handling capability for debug images.
+  if [[ "$(systemctl show -p LoadState systemd-logind.service 2>/dev/null)" == "LoadState=loaded" ]]; then
+    mkdir -p /etc/systemd/logind.conf.d
+    cp /usr/share/oem/confidential_space/logind_override.conf /etc/systemd/logind.conf.d/logind_override.conf
+    systemctl restart systemd-logind.service
+  fi
 
   # Override default system-stats-monitor.json for node-problem-detector.
   cp /usr/share/oem/confidential_space/system-stats-monitor-cs.json /etc/node_problem_detector/system-stats-monitor.json
--- launcher/image/preload.sh
+++ launcher/image/preload.sh
@@ -24,6 +24,7 @@ copy_experiment_client() {
 setup_launcher_systemd_unit() {
   cp container-runner.service "${CS_PATH}/container-runner.service"
   cp exit_script.sh "${CS_PATH}/exit_script.sh"
+  cp logind_override.conf "${CS_PATH}/logind_override.conf"
 }
 
 append_cmdline() {
