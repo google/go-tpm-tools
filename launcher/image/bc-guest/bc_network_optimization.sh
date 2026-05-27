1e40ef70e1c7637c0140e53618f47598b43f1a09
jacobdw <jacobdw@google.com>
2026-05-27 14:35:13 -0700

    fixing optimization script

--- launcher/image/bc-guest/bc_network_optimization.sh
+++ launcher/image/bc-guest/bc_network_optimization.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Disable XPS
-echo 0 | tee /sys/class/net/eth0/queues/tx*/xps_cpus
-echo 0 | tee /sys/class/net/eth1/queues/tx*/xps_cpus
-
-# NUMA Node enlightment
-echo 0 > /sys/class/net/eth0/device/numa_node
-echo 1 > /sys/class/net/eth1/device/numa_node
-
 # Ethtool optimizations
+# Perform these first - setting ring length will reset other settings
 ethtool -G eth0 rx 2048 tx 2048
 ethtool -G eth1 rx 2048 tx 2048
 ethtool -C eth0 adaptive-rx off adaptive-tx off rx-usecs 20 tx-usecs 65
@@ -20,6 +13,14 @@ ethtool -G eth1 tcp-data-split off
 sysctl -w net.core.netdev_budget=600
 sysctl -w net.core.netdev_budget_usecs=4000
 
+# Disable XPS
+echo 0 | tee /sys/class/net/eth0/queues/tx*/xps_cpus 2>/dev/null || true
+echo 0 | tee /sys/class/net/eth1/queues/tx*/xps_cpus 2>/dev/null || true
+
+# NUMA Node enlightment
+echo 0 | tee /sys/class/net/eth0/device/numa_node 2>/dev/null || true
+echo 1 | tee /sys/class/net/eth1/device/numa_node 2>/dev/null || true
+
 # IRQ smp affinity optimizations
-echo 40-55 | tee /proc/irq/*/idpf-eth0*/../smp_affinity_list > /dev/null
-echo 96-111 | tee /proc/irq/*/idpf-eth1*/../smp_affinity_list > /dev/null
+echo 40-55 | tee /proc/irq/*/idpf-eth0*/../smp_affinity_list 2>/dev/null || true
+echo 96-111 | tee /proc/irq/*/idpf-eth1*/../smp_affinity_list 2>/dev/null || true
