0acabcbe916a723854a978899b3bc23ebf7ad2c5
meetrajvala <160713120+meetrajvala@users.noreply.github.com>
2026-06-02 11:01:06 +0530

    Integrate Launcher with new Key Protection Module server (#798)
    * launch wsd server with new key protection module
    
    * update way of launching wsd
    
    * update wsd container image ref
    
    * fix cloudbuild argument
    
    * fix wsd directory path
    
    * address review comments
    
    * fix format and image downloading
    
    * move kps_vm_ip to env file and update systemd file
    
    * fix wsd runner
    
    * add kps_ip env

---
+++ launcher/image/bc-guest/wsd/wsd_runner.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [ -f /usr/share/oem/wsd/image.env ]; then
+    source /usr/share/oem/wsd/image.env
+else
+    echo "Error: Config file not found!"
+    exit 1
+fi
+
+echo "=== Launching Workload Service Daemon Container ==="
+
+echo "Importing image from ${IMAGE_PATH}..."
+if [ -f "$IMAGE_PATH" ]; then
+    ctr images import "$IMAGE_PATH"
+else
+    echo "Error: Image file not found at $IMAGE_PATH"
+    exit 1
+fi
+
+echo "Checking for existing container..."
+if ctr container info "$CONTAINER_NAME" >/dev/null 2>&1; then
+    echo "Removing existing container..."
+    ctr container rm "$CONTAINER_NAME"
+fi
+
+ctr run --rm --net-host --mount "type=bind,src=/tmp/container_launcher/,dst=/run/container_launcher/,options=rbind:rw" --env SERVICE_ROLE="SERVICE_ROLE_WSD" --env KEY_PROTECTION_MECHANISM="KEY_PROTECTION_VM" --env KPS_IP="${KPS_VM_IP}"  "$IMAGE_REF" "$CONTAINER_NAME" agent --kps-vm-ip="${KPS_VM_IP}" --socket="/run/container_launcher/kmaserver.sock"
