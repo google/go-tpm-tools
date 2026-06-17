894d6d088ad529bcc73534c2e1b03a6c8b44ac4b
Jiankun Lü <jiankun@google.com>
2026-06-16 23:18:21 -0700

    [launcher] add gpu support for bc guest (#828)
    Add gpu daemon image
    
    Co-authored-by: Sibghat Shah <sibghat@google.com>

--- launcher/image/bc-guest/bcentrypoint.sh
+++ launcher/image/bc-guest/bcentrypoint.sh
@@ -35,6 +35,38 @@ main() {
   fi
 
   systemctl daemon-reload
+
+  # Install gpu drivers
+  modprobe ib_umad
+  modprobe nvidia
+  modprobe nvidia-uvm
+  modprobe nvidia-modeset
+
+  echo "Running nvidia-persistenced" | tee /dev/console
+  systemd-run  -p Type=forking --unit=nvidia-persistenced-transient /opt/nvidia/590.48.01/bin/nvidia-persistenced
+
+  echo "Waiting 1 minute for nvidia-persistenced to initialize..." | tee /dev/console
+  sleep 60s
+
+  if [ -f  /usr/share/oem/confidential_space/gpu_helper.tar ]; then
+      echo "Importing GPU helper image..." | tee /dev/console
+      sudo ctr images import /usr/share/oem/confidential_space/gpu_helper.tar
+  fi
+
+  echo "Running NVLSM and Fabric Manager..." | tee /dev/console
+
+  sudo ctr containers create --privileged --net-host \
+      --mount type=bind,src=/dev,dst=/dev,options=rbind:rw \
+      --mount type=bind,src=/opt/nvidia,dst=/opt/nvidia-host,options=rbind:rw \
+      docker.io/library/guest-gpu-tools:latest \
+      guest-gpu-tools-task
+
+  sudo ctr tasks start -d guest-gpu-tools-task
+  echo "Waiting 2 min for NVLSM and Fabric Manager to initialize..." | tee /dev/console
+  sleep 2m
+
+  echo "GPU daemon ready" | tee /dev/console
+
   systemctl enable container-runner.service
   systemctl enable wsd.service
   systemctl start container-runner.service
---
+++ launcher/internal/gpu/daemons/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+echo "Starting guest GPU daemon service [590.48.01]..." | tee /dev/console
+export LD_LIBRARY_PATH=/opt/nvidia-host/590.48.01/lib64:$LD_LIBRARY_PATH
+
+# Modify config
+sed -i 's/PARTITION_RAIL_POLICY=greedy/PARTITION_RAIL_POLICY=symmetric/' /usr/share/nvidia/nvswitch/fabricmanager.cfg
+
+# Get port GUID
+export port_guid="$(ibstat | grep -oE '(GUID:\ )(0x[a-z0-9]+)' | grep -oE -m 1 '0x[a-z0-9]+')"
+echo "Using Port GUID: ${port_guid}" | tee /dev/console
+
+# Start nvlsm
+/opt/nvidia/nvlsm/sbin/nvlsm -F /usr/share/nvidia/nvlsm/nvlsm.conf 2>&1 | tee /dev/console &
+
+# Start fabricmanager
+/usr/bin/nv-fabricmanager -c /usr/share/nvidia/nvswitch/fabricmanager.cfg -g ${port_guid} 2>&1 | tee /dev/console &
+# Wait for fabric manager to initialize
+echo "Waiting for fabric manager to initialize..." | tee /dev/console
+sleep 10
+
+# Check readiness of the fabric
+echo "Checking fabric readiness..." | tee /dev/console
+for i in {1..15}; do
+    fabric_output=$(nvidia-smi -q | grep -E '^\s*Fabric$' -A 1)
+    echo "$fabric_output" | tee /dev/console
+    
+    fabric_count=$(echo "$fabric_output" | grep -c "Fabric")
+    completed_count=$(echo "$fabric_output" | grep -c "Completed")
+    
+    if [ "$fabric_count" -gt 0 ] && [ "$fabric_count" -eq "$completed_count" ]; then
+        echo "Fabric readiness completed for all GPUs." | tee /dev/console
+        break
+    fi
+    
+    echo "Fabric not ready yet (Completed: $completed_count/$fabric_count). Retrying in 2s..." | tee /dev/console
+    sleep 2
+done
+
+# Keep container running by sleeping infinity, since background tasks might fork and exit
+echo "Services started. Waiting indefinitely..." | tee /dev/console
+sleep infinity
