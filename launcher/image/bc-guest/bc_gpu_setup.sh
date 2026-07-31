0a2284167a0b71701e73e68d05ba165cd5e57e7c
mayafleischer <mfleischer@google.com>
2026-07-31 21:34:41 +0000

    clean up unused gpu code

--- launcher/image/bc-guest/bc_gpu_setup.sh
+++ launcher/image/bc-guest/bc_gpu_setup.sh
@@ -13,24 +13,6 @@ systemd-run -p Type=forking --unit=nvidia-persistenced-transient /opt/nvidia/595
 echo "Waiting 1 minute for nvidia-persistenced to initialize..." | tee /dev/console
 sleep 60s
 
-if [ ! -d /usr/share/oem/gpu_helper/rootfs ]; then
-    echo "Error: GPU rootfs not found at /usr/share/oem/gpu_helper/rootfs!" | tee /dev/console
-    exit 1
-fi
 
-# echo "Running NVLSM and Fabric Manager..." | tee /dev/console
-
-# sudo ctr containers create --rootfs --privileged --net-host \
-#     --mount type=bind,src=/dev,dst=/dev,options=rbind:rw \
-#     --mount type=bind,src=/opt/nvidia,dst=/opt/nvidia-host,options=rbind:rw \
-#     /usr/share/oem/gpu_helper/rootfs \
-#     guest-gpu-tools-task \
-#     /entrypoint.sh
-
-# sudo ctr tasks start -d guest-gpu-tools-task
-# echo "Waiting 2 min for NVLSM and Fabric Manager to initialize..." | tee /dev/console
-# sleep 2m
-
-echo "Skipping NVLSM and Fabric Manager" | tee /dev/console
 
 echo "GPU daemon ready" | tee /dev/console
