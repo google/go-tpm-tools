f5fd00273d8e54ad648c7541f96e048f06f7252c
Sibghat Shah <sibghat@google.com>
2026-08-03 16:48:17 -0700

    Refactor ContainerRunner to call the attestation agent for GPU Attestation (#707)
    * Refactor device ROT attestation and decouple AttestationAgent (#707)
    
    Co-authored-by: yawangwang <yawangwang@google.com>
    
    * Address PR feedback: return concrete *NvidiaAttester, validate uniform accelerator ROTs, and clean up nonce read
    
    ---------
    
    Co-authored-by: yawangwang <yawangwang@google.com>

--- agent/agent.go
+++ agent/agent.go
@@ -35,6 +35,7 @@ import (
 	"github.com/GoogleCloudPlatform/confidential-space/server/labels"
 	attestationpb "github.com/GoogleCloudPlatform/confidential-space/server/proto/gen/attestation"
 	hostservicepb "github.com/GoogleCloudPlatform/confidential-space/server/proto/gen/hostservice"
+	"github.com/google/go-tpm-tools/agent/device"
 	"github.com/google/go-tpm-tools/cel"
 	"github.com/google/go-tpm-tools/client"
 	"github.com/google/go-tpm-tools/internal"
@@ -88,16 +89,6 @@ type attestRoot interface {
 	Attest(nonce []byte) (any, error)
 	// ComputeNonce hashes the challenge and extraData using the algorithm preferred by the attestation root.
 	ComputeNonce(challenge []byte, extraData []byte) []byte
-	// AddDeviceROTs adds detected device RoTs(root of trust).
-	AddDeviceROTs([]DeviceROT)
-	// AttestDeviceROTs fetches a list of runtime device attestation report.
-	AttestDeviceROTs(nonce []byte) ([]any, error)
-}
-
-// DeviceROT defines an interface for all attached devices to collect attestation.
-type DeviceROT interface {
-	// Attest fetches an attestation from the attached device detected by launcher.
-	Attest(nonce []byte) (any, error)
 }
 
 // AttestAgentOpts contains user generated options when calling the
@@ -109,9 +100,7 @@ type AttestAgentOpts struct {
 }
 
 // DeviceReportOpts contains options for runtime device attestations.
-type DeviceReportOpts struct {
-	EnableRuntimeGPUAttestation bool
-}
+type DeviceReportOpts = device.ReportOpts
 
 // AcpiOpts contains options for platform ACPI data.
 type AcpiOpts struct {
@@ -141,6 +130,7 @@ type agent struct {
 	logger           Logger
 	sigsCache        *sigsCache
 	signedImageRepos []string
+	deviceROTManager *device.ROTManager
 }
 
 type bcAgent struct {
@@ -154,9 +144,9 @@ type bcAgent struct {
 // - principalFetcher is a func to fetch GCE principal tokens for a given audience.
 // - signaturesFetcher is a func to fetch container image signatures associated with the running workload.
 // - logger will log any partial errors returned by VerifyAttestation.
-func CreateAttestationAgent(tpm io.ReadWriteCloser, akFetcher util.TpmKeyFetcher, verifierClient verifier.Client, principalFetcher principalIDTokenFetcher, sigsFetcher SignatureFetcher, exps Experiments, logger Logger, deviceROTs []DeviceROT, signedImageRepos []string) (AttestationAgent, error) {
+func CreateAttestationAgent(tpm io.ReadWriteCloser, akFetcher util.TpmKeyFetcher, verifierClient verifier.Client, principalFetcher principalIDTokenFetcher, sigsFetcher SignatureFetcher, exps Experiments, logger Logger, deviceROTManager *device.ROTManager, signedImageRepos []string) (AttestationAgent, error) {
 	if exps.BcMode {
-		return createBCAgent(principalFetcher, sigsFetcher, exps, logger, deviceROTs, signedImageRepos)
+		return createBCAgent(principalFetcher, sigsFetcher, exps, logger, deviceROTManager, signedImageRepos)
 	}
 
 	// Fetched the AK and save it, so the agent doesn't need to create a new key everytime
@@ -211,12 +201,15 @@ func CreateAttestationAgent(tpm io.ReadWriteCloser, akFetcher util.TpmKeyFetcher
 		logger.Info("Using TPM PCR as attestation root.")
 	}
 
-	// Add deviceRoTs to the CPU attestation root.
-	attestAgent.avRot.AddDeviceROTs(deviceROTs)
+	if deviceROTManager == nil {
+		deviceROTManager = device.NewROTManager(nil)
+	}
+	attestAgent.deviceROTManager = deviceROTManager
+
 	return attestAgent, nil
 }
 
-func createBCAgent(principalFetcher principalIDTokenFetcher, sigsFetcher SignatureFetcher, exps Experiments, logger Logger, deviceROTs []DeviceROT, signedImageRepos []string) (AttestationAgent, error) {
+func createBCAgent(principalFetcher principalIDTokenFetcher, sigsFetcher SignatureFetcher, exps Experiments, logger Logger, deviceROTManager *device.ROTManager, signedImageRepos []string) (AttestationAgent, error) {
 	logger.Info("Running in BC mode, skipping Attestation Key fetching.")
 
 	baseAgent := &agent{
@@ -236,7 +229,7 @@ func createBCAgent(principalFetcher principalIDTokenFetcher, sigsFetcher Signatu
 		return nil, fmt.Errorf("running in BC mode but TDX not supported")
 	}
 
-	baseAgent.avRot.AddDeviceROTs(deviceROTs)
+	baseAgent.deviceROTManager = deviceROTManager
 	return &bcAgent{agent: baseAgent}, nil
 }
 
@@ -524,10 +517,10 @@ func getAcpiData() (*attestationpb.AcpiData, error) {
 }
 
 func (a *agent) attestDeviceROTs(nonce []byte, opts AttestAgentOpts) ([]*attestationpb.DeviceAttestationReport, error) {
-	if opts.DeviceReportOpts == nil {
+	if opts.DeviceReportOpts == nil || a.deviceROTManager == nil {
 		return nil, nil
 	}
-	deviceROTs, err := a.avRot.AttestDeviceROTs(nonce)
+	deviceROTs, err := a.deviceROTManager.AttestDeviceROTs(nonce, *opts.DeviceReportOpts)
 	if err != nil {
 		return nil, err
 	}
@@ -536,13 +529,11 @@ func (a *agent) attestDeviceROTs(nonce []byte, opts AttestAgentOpts) ([]*attesta
 	for _, dr := range deviceROTs {
 		switch v := dr.(type) {
 		case *attestationpb.NvidiaAttestationReport:
-			if opts.DeviceReportOpts.EnableRuntimeGPUAttestation {
-				deviceReports = append(deviceReports, &attestationpb.DeviceAttestationReport{
-					Report: &attestationpb.DeviceAttestationReport_NvidiaReport{
-						NvidiaReport: v,
-					},
-				})
-			}
+			deviceReports = append(deviceReports, &attestationpb.DeviceAttestationReport{
+				Report: &attestationpb.DeviceAttestationReport_NvidiaReport{
+					NvidiaReport: v,
+				},
+			})
 		}
 	}
 	return deviceReports, nil
@@ -575,12 +566,11 @@ func convertOCIToContainerSignature(ociSig oci.Signature) (*verifier.ContainerSi
 }
 
 type tpmAttestRoot struct {
-	tpmMu      sync.Mutex
-	fetchedAK  *client.Key
-	tpm        io.ReadWriteCloser
-	cosCel     gecel.CEL
-	hashAlgos  []crypto.Hash
-	deviceROTs []DeviceROT
+	tpmMu     sync.Mutex
+	fetchedAK *client.Key
+	tpm       io.ReadWriteCloser
+	cosCel    gecel.CEL
+	hashAlgos []crypto.Hash
 }
 
 func (t *tpmAttestRoot) GetCEL() gecel.CEL {
@@ -621,22 +611,10 @@ func (t *tpmAttestRoot) ComputeNonce(challenge []byte, extraData []byte) []byte 
 	return finalNonce[:]
 }
 
-func (t *tpmAttestRoot) AddDeviceROTs(deviceROTs []DeviceROT) {
-	t.deviceROTs = append(t.deviceROTs, deviceROTs...)
-}
-
-func (t *tpmAttestRoot) AttestDeviceROTs(nonce []byte) ([]any, error) {
-	t.tpmMu.Lock()
-	defer t.tpmMu.Unlock()
-
-	return doAttestDeviceROTs(t.deviceROTs, nonce)
-}
-
 type tdxAttestRoot struct {
-	tdxMu      sync.Mutex
-	qp         *tg.LinuxConfigFsQuoteProvider
-	cosCel     gecel.CEL
-	deviceROTs []DeviceROT
+	tdxMu  sync.Mutex
+	qp     *tg.LinuxConfigFsQuoteProvider
+	cosCel gecel.CEL
 }
 
 func (t *tdxAttestRoot) GetCEL() gecel.CEL {
@@ -687,13 +665,6 @@ func (t *tdxAttestRoot) Attest(nonce []byte) (any, error) {
 	}, nil
 }
 
-func (t *tdxAttestRoot) AttestDeviceROTs(nonce []byte) ([]any, error) {
-	t.tdxMu.Lock()
-	defer t.tdxMu.Unlock()
-
-	return doAttestDeviceROTs(t.deviceROTs, nonce)
-}
-
 func (t *tdxAttestRoot) ComputeNonce(challenge []byte, extraData []byte) []byte {
 	challengeData := challenge
 	if extraData != nil {
@@ -703,10 +674,6 @@ func (t *tdxAttestRoot) ComputeNonce(challenge []byte, extraData []byte) []byte 
 	challengeDigest := sha512.Sum512(challengeData)
 	finalNonce := sha512.Sum512(append([]byte(labels.WorkloadAttestation), challengeDigest[:]...))
 	return finalNonce[:]
-}
-
-func (t *tdxAttestRoot) AddDeviceROTs(deviceROTs []DeviceROT) {
-	t.deviceROTs = append(t.deviceROTs, deviceROTs...)
 }
 
 // Refresh refreshes the internal state of the attestation agent.
@@ -806,16 +773,4 @@ func convertToTPMQuote(v *pb.Attestation) *attestationpb.TpmQuote {
 			},
 		},
 	}
-}
-
-func doAttestDeviceROTs(deviceROTs []DeviceROT, nonce []byte) ([]any, error) {
-	var deviceReports []any
-	for _, deviceROT := range deviceROTs {
-		deviceReport, err := deviceROT.Attest(nonce)
-		if err != nil {
-			return nil, err
-		}
-		deviceReports = append(deviceReports, deviceReport)
-	}
-	return deviceReports, nil
 }
--- launcher/image/test/scripts/gpu/test_gpu_workload.sh
+++ launcher/image/test/scripts/gpu/test_gpu_workload.sh
@@ -16,3 +16,16 @@ else
     echo 'TEST FAILED.' > /workspace/status.txt
     print_serial=true
 fi
+
+if echo $SERIAL_OUTPUT | grep -q 'Successfully measured device attestation binding events and enabled ready states'
+then
+    echo "- GPU attestation collection verified"
+else
+    echo "FAILED: GPU attestation not collected"
+    echo 'TEST FAILED.' > /workspace/status.txt
+    print_serial=true
+fi
+
+if $print_serial; then
+    echo "$SERIAL_OUTPUT"
+fi
