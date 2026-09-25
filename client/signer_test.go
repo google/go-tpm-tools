e62eef4e6a440ca02a72dc30e0d189569bbbcd9f
Brennan Tracy <brennantracy@google.com>
2026-09-25 21:31:11 +0000

    client: deduplicate key creation in signing tests
    Reuse instantiated signing keys across multiple signature operations instead of creating distinct keys for each signing method. In PSS signing tests, consolidate duplicate key definitions and test both raw hash and PSS option variants against the same key instance.
    
    Provisioning RSA primary keys within the TPM simulator requires expensive key generation cycles that dominate unit test runtime. Because signing operations are stateless and non-destructive with respect to the loaded TPM key context, performing multiple signing verifications on a single key preserves test coverage while cutting test execution time by more than half.

--- client/signer_test.go
+++ client/signer_test.go
@@ -117,17 +117,8 @@ func TestSign(t *testing.T) {
 			if !k.verify(signer.Public(), k.hash, digest, sig) {
 				t.Error(err)
 			}
-		})
-		t.Run(k.name+"-SignData", func(t *testing.T) {
-			test.SkipOnUnsupportedAlg(t, rwc, alg)
 
-			key, err := client.NewKey(rwc, tpm2.HandleEndorsement, k.template)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer key.Close()
-
-			sig, err := key.SignData(message)
+			sig, err = key.SignData(message)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -203,30 +194,20 @@ func TestSignPSS(t *testing.T) {
 	defer client.CheckedClose(t, rwc)
 	keys := []struct {
 		name     string
-		opts     crypto.SignerOpts
+		hash     crypto.Hash
 		template tpm2.Public
 		keyBits  uint16
-		saltLen  int
 	}{
-		// saltLen should be (keyBits/8) - digestSize - 2, unless that is less than
-		// digestSize in which case, saltLen will be digestSize.
-		// The only normal case where saltLen is not digestSize is when using
-		// 1024 keyBits with SHA512.
-		{"RSA-SHA1", crypto.SHA1, templatePSS(tpm2.AlgSHA1), 1024, 20},
-		{"RSA-SHA256", crypto.SHA256, templatePSS(tpm2.AlgSHA256), 1024, 32},
-		{"RSA-SHA384", crypto.SHA384, templatePSS(tpm2.AlgSHA384), 1024, 48},
-		{"RSA-SHA512", crypto.SHA512, templatePSS(tpm2.AlgSHA512), 1024, 62},
-		{"RSA-SHA512", crypto.SHA512, templatePSS(tpm2.AlgSHA512), 2048, 64},
-		{"RSA-SHA1", &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthAuto, Hash: crypto.SHA1}, templatePSS(tpm2.AlgSHA1), 1024, 20},
-		{"RSA-SHA256", &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthAuto, Hash: crypto.SHA256}, templatePSS(tpm2.AlgSHA256), 1024, 32},
-		{"RSA-SHA384", &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthAuto, Hash: crypto.SHA384}, templatePSS(tpm2.AlgSHA384), 1024, 48},
-		{"RSA-SHA512", &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthAuto, Hash: crypto.SHA512}, templatePSS(tpm2.AlgSHA512), 1024, 62},
-		{"RSA-SHA512", &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthAuto, Hash: crypto.SHA512}, templatePSS(tpm2.AlgSHA512), 2048, 64},
+		{"RSA-SHA1", crypto.SHA1, templatePSS(tpm2.AlgSHA1), 1024},
+		{"RSA-SHA256", crypto.SHA256, templatePSS(tpm2.AlgSHA256), 1024},
+		{"RSA-SHA384", crypto.SHA384, templatePSS(tpm2.AlgSHA384), 1024},
+		{"RSA-SHA512", crypto.SHA512, templatePSS(tpm2.AlgSHA512), 1024},
+		{"RSA-SHA512-2048", crypto.SHA512, templatePSS(tpm2.AlgSHA512), 2048},
 	}
 
 	for _, k := range keys {
 		t.Run(k.name, func(t *testing.T) {
-			alg, err := tpm2.HashToAlgorithm(k.opts.HashFunc())
+			alg, err := tpm2.HashToAlgorithm(k.hash)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -240,7 +221,7 @@ func TestSignPSS(t *testing.T) {
 			}
 			defer key.Close()
 
-			hash := k.opts.HashFunc().New()
+			hash := k.hash.New()
 			hash.Write([]byte("authenticated message"))
 			digest := hash.Sum(nil)
 
@@ -248,15 +229,21 @@ func TestSignPSS(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			sig, err := signer.Sign(nil, digest[:], k.opts)
-			if err != nil {
-				t.Fatal(err)
+			optsList := []crypto.SignerOpts{
+				k.hash,
+				&rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthAuto, Hash: k.hash},
 			}
-			// Different implementations may specify different salt length. Some have "keyBytes - digestSize - 2", some have
-			// just "digestSize". Therefore here we just verify with default salt length.
-			err = rsa.VerifyPSS(signer.Public().(*rsa.PublicKey), k.opts.HashFunc(), digest[:], sig, nil)
-			if err != nil {
-				t.Error(err)
+			for _, opts := range optsList {
+				sig, err := signer.Sign(nil, digest, opts)
+				if err != nil {
+					t.Fatal(err)
+				}
+				// Different implementations may specify different salt length. Some have "keyBytes - digestSize - 2", some have
+				// just "digestSize". Therefore here we just verify with default salt length.
+				err = rsa.VerifyPSS(signer.Public().(*rsa.PublicKey), k.hash, digest, sig, nil)
+				if err != nil {
+					t.Error(err)
+				}
 			}
 		})
 	}
